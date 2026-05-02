f4304d4fa6af747d6bc9efb5b782a3f0c5e8d301
eliyya <tecno_eli@hotmail.com>
2026-05-01 19:04:42 -0600

    config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
+import { AppController } from './app.controller.js'
 import { AppService } from './app.service'
 import { RoutesModule } from './routes/routes.module'
 import { PrismaService } from './prisma/prisma.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-    imports: [RoutesModule],
+    imports: [
+        RoutesModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService, PrismaService],
 })
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './generated/prisma/client'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-    constructor() {
-        const adapter = new PrismaPg(process.env.DATABASE_URL!)
-        super({ adapter })
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
+    constructor(configService: ConfigService) {
+        super({
+            adapter: new PrismaPg(configService.get<string>('DATABASE_URL')),
+        })
+    }
+
+    async onModuleInit() {
+        await this.$connect()
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect()
     }
 }
