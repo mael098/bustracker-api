d46cc127e41058f45a10ed36f240d645e8b78bc3
eliyya <tecno_eli@hotmail.com>
2026-05-02 22:14:34 -0600

    remove log

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 
-console.log('envs', process.env.NODE_ENV)
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.useGlobalPipes(
