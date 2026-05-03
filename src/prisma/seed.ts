6d3e525e7707e9e79baae45cb9ac0fc439690117
eliyya <tecno_eli@hotmail.com>
2026-05-03 00:00:55 -0600

    fix seed

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,6 +1,6 @@
 // @eslint-ignore
 import { readFileSync } from 'node:fs'
-import { PrismaClient } from './generated/prisma/client.ts'
+import { PrismaClient } from './generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
